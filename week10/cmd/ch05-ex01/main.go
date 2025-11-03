66536a22a8869e11ff80f3a5e6660381ba8c8c43
inhadeepblue <deepblue@inha.ac.kr>
2025-11-03 12:04:49 +0900

    week10 array v0.5

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -5,13 +5,12 @@ import (
 )
 
 func main() {
-	arrayBool := [3]bool{true, false, true}
+	arrayBool := [2]bool{true, false}
 	arrayInt := [3]int{-9, 11, 7}
-	for i := 0; i < 3; i++ {
+	for i := 0; i < len(arrayInt); i++ {
 		fmt.Println(i, arrayInt[i])
-		fmt.Println(i, arrayBool[i])
+		//fmt.Println(i, arrayBool[i])  // runtime error
 	}
-	// fmt.Println(reflect.TypeOf(arrayBool))
-	// fmt.Printf("%#v\n", arrayBool)
-	// fmt.Printf("%#v\n", arrayInt)
+	fmt.Printf("%#v\n", arrayBool)
+	fmt.Printf("%#v\n", arrayInt)
 }
