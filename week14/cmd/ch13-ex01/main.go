b64bbb9a2d8e5eb436ddb10e533b3704f0dfa1d0
inhadeepblue <deepblue@inha.ac.kr>
2025-12-01 12:03:00 +0900

    week14 goroutine v0.3

--- week14/cmd/ch13-ex01/main.go
+++ week14/cmd/ch13-ex01/main.go
@@ -17,6 +17,6 @@ func main() {
 	say("고루틴") // 새 고루틴에서 실행
 	say("메인")  // 메인 고루틴에서 실행
 
-	time.Sleep(1 * time.Second)
+	//time.Sleep(1 * time.Second)
 	fmt.Println("전체 실행 시간 : ", time.Since(start))
 }
